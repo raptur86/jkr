2337991fe53b202089cd12b388303a1f28d0d69a
kyerim1 <nilzero25@gmail.com>
2024-01-18 17:52:49 +0900

    18

--- eternal/js/main.js
+++ eternal/js/main.js
@@ -1,4 +1,43 @@
 $("#cashCheck").on("click",function(){  
     $("#write_modal").show(); 
+    $("#useFm").attr("action","");
+    $(".credit_write").addClass("noneAttr");
+    $(".credit_dvdWrite").addClass("noneAttr");
+    $(".loans_write").addClass("noneAttr");
     $(".cash_write").removeClass("noneAttr");
 })
+
+$("#credit").on("click",function(){
+    $("#write_modal").show(); 
+    $("#useFm").attr("action","");
+    $(".cash_write").addClass("noneAttr");
+    $(".credit_dvdWrite").addClass("noneAttr");
+    $(".loans_write").addClass("noneAttr");
+    $(".credit_write").removeClass("noneAttr");
+});
+$("#creditDivAdd").on("click",function(){ 
+    $("#write_modal").show(); 
+    $("#useFm").attr("action","");
+    $(".cash_write").addClass("noneAttr");
+    $(".credit_write").addClass("noneAttr");
+    $(".loans_write").addClass("noneAttr");
+    $(".credit_dvdWrite").removeClass("noneAttr");
+});
+$("#loansAdd").on("click",function(){ 
+    $("#write_modal").show(); 
+    $("#useFm").attr("action","");
+    $(".cash_write").addClass("noneAttr");
+    $(".credit_write").addClass("noneAttr");
+    $(".credit_dvdWrite").addClass("noneAttr");
+    $(".loans_write").removeClass("noneAttr");
+});
+
+$("#write_bg").on("click",function(){
+    $("#write_modal").hide();
+});
+
+$(".shell label").each(function(i,v){
+    var len = $(this).text().length;
+    if(len>2)
+        $(this).css("font-size",50/len+"px");
+});
